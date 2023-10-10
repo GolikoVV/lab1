ea3e06218903391ea843708708acf449a59c9539
Valeria <112738969+GolikoVV@users.noreply.github.com>
2023-10-10 12:48:09 +0700

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите максимальный размер стека:");
+            int maxSize = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>(maxSize);
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить элемент в стек");
+                Console.WriteLine("2. Извлечь элемент из стека");
+                Console.WriteLine("3. Посмотреть верхний элемент стека (Peek)");
+                Console.WriteLine("4. Вывести среднее арифметическое");
+                Console.WriteLine("5. Создать копию стека");
+                Console.WriteLine("6. Печать содержимого стека");
+                Console.WriteLine("7. Выйти из программы");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите значение для добавления в стек:");
+                        int valueToAdd = int.Parse(Console.ReadLine());
+                        try
+                        {
+                            stack.Push(valueToAdd);
+                            Console.WriteLine($"Добавлено в стек: {valueToAdd}");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Стек переполнен. Невозможно добавить элемент.");
+                        }
+                        break;
+
+                    case 2:
+                        try
+                        {
+                            int poppedValue = stack.Pop();
+                            Console.WriteLine($"Извлечено из стека: {poppedValue}");
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Console.WriteLine("Стек пуст. Невозможно извлечь элемент.");
+                        }
+                        break;
+
+                    case 3:
+                        try
+                        {
+                            int peekedValue = stack.Peek();
+                            Console.WriteLine($"Верхний элемент стека: {peekedValue}");
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Console.WriteLine("Стек пуст. Невозможно просмотреть элемент.");
+                        }
+                        break;
+
+                    case 4:
+                        try
+                        {
+                            double average = stack.Average();
+                            Console.WriteLine($"Среднее арифметическое элементов стека: {average}");
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Console.WriteLine("Стек пуст. Невозможно вычислить среднее.");
+                        }
+                        break;
+
+                    case 5:
+                        Stack<int> copyStack = new Stack<int>(stack);
+                        Console.WriteLine("Создана копия стека.");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Содержимое стека:");
+
+                        break;
+
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ lab1/Stack.cs
@@ -0,0 +1,134 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    namespace StackExample
+    {
+    //  Это класс, представляющий узел стека. Узел содержит данные (Data) и
+    //  ссылку на следующий узел (Next).
+    //  Этот класс используется для хранения элементов стека.
+    class StackNode<T>
+        {
+            public T Data { get; set; } // Публичное свойство для хранения данных элемента стека
+            public StackNode<T> Next { get; set; } // Ссылка на следующий элемент стека
+
+            public StackNode(T data)
+            {
+                Data = data; // Конструктор для создания нового узла стека с данными
+                Next = null; // Изначально нет следующего элемента
+            }
+        }
+
+        // Класс стека
+        class Stack<T>
+        {
+            private StackNode<T> top; //  ссылка на вершину стека 
+        private int capacity; // Максимальная вместимость стека
+            private int count; // Текущее количество элементов в стеке
+            private double sum; // Сумма элементов для вычисления среднего
+
+            // Конструктор для создания нового (пустого) стека по размеру
+            public Stack(int capacity)
+            {
+                this.capacity = capacity; // Устанавливаем максимальную вместимость
+                this.count = 0; // Изначально стек пуст
+                this.top = null; // Вершина указывает на null, так как стек пуст
+                this.sum = 0; // Изначально сумма равна нулю
+            }
+
+            // Конструктор для создания копии другого стека
+            public Stack(Stack<T> otherStack)
+            {
+                this.capacity = otherStack.capacity; // Устанавливаем максимальную вместимость
+                this.count = 0; // Изначально стек пуст
+                this.top = null; // Вершина указывает на null, так как стек пуст
+                this.sum = 0; // Изначально сумма равна нулю
+
+                StackNode<T> current = otherStack.top; // Начинаем с вершины другого стека
+                while (current != null)
+                {
+                    Push(current.Data); // Копируем элементы из другого стека
+                    current = current.Next;
+                }
+            }
+
+            // Метод для добавления нового элемента в стек
+            public void Push(T data)
+            {
+                if (count >= capacity)
+                {
+                    throw new OverflowException("Стек переполнен.");
+                }
+
+                StackNode<T> newNode = new StackNode<T>(data); // Создаем новый узел
+                newNode.Next = top; // Устанавливаем новый узел как вершину стека
+                top = newNode;
+
+                sum += Convert.ToDouble(data); // Добавляем значение в сумму для вычисления среднего
+                count++; // Увеличиваем количество элементов в стеке
+            }
+
+            // Метод для извлечения элемента из стека (и удаления его)
+            public T Pop()
+            {
+                if (count <= 0)
+                {
+                    throw new InvalidOperationException("Стек пуст.");
+                }
+                StackNode<T> prev = top;
+                T data = top.Data; // Получаем данные верхнего элемента
+                top = top.Next; // Удаляем верхний элемент из стека
+                prev.Next = null;
+
+                sum -= Convert.ToDouble(data); // Вычитаем значение из суммы
+                count--; // Уменьшаем количество элементов
+
+                return data; // Возвращаем удаленное значение
+            }
+
+            // Метод для возврата элемента без его удаления
+            public T Peek()
+            {
+                if (count <= 0)
+                {
+                    throw new InvalidOperationException("Стек пуст.");
+                }
+
+                return top.Data; // Возвращаем данные верхнего элемента
+            }
+
+            // Метод для проверки на переполнение стека
+            public bool IsFull()
+            {
+                return count >= capacity; // Стек полон, если количество элементов достигло максимальной вместимости
+            }
+
+            // Метод для печати содержимого стека
+            public void Print()
+            {
+                StackNode<T> current = top; // Начинаем с вершины стека
+                while (current != null)
+                {
+                    Console.Write($"{current.Data} ");
+                    current = current.Next;
+                }
+                Console.WriteLine();
+            }
+
+            // Метод для вычисления среднего арифметического элементов стека
+            public double GetAverage()
+            {
+                if (count == 0)
+                {
+                    return 0; // Если стек пуст, среднее значение равно 0
+                }
+
+                return sum / count; // Среднее арифметическое 
+            }
+        }
+    }
+
+
